934d55a3135bd37e2a9e44a98f23c166efc16e78
Lucas <lucaspedrazoli@gmail.com>
2020-09-26 14:11:36 -0300

    log repeated insertions with balanced tree

--- Starter.playground/Contents.swift
+++ Starter.playground/Contents.swift
@@ -1,4 +1,10 @@
 // Copyright (c) 2019 Razeware LLC
 // For full license & permission details, see LICENSE.markdown.
 
-print("a")
+example(of: "repeated insertions") {
+  var tree = AVLTree<Int>()
+  for i in 0..<15 {
+    tree.insert(i)
+  }
+  print(tree)
+}
